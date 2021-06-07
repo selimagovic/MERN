f9023d7a8028839c61c82a1c310a917f80607a05
Selim Agovic <selimagovic@gmail.com>
2021-06-07 12:12:10 +0200

    Added a backed

--- ProShop App/backend/server.js
+++ ProShop App/backend/server.js
@@ -1,17 +1,26 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
 
-const app = express()
-app.listen(5000, console.log('server running on port 5000'))
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Api is running...')
-})
+  res.send('Api is running...');
+});
+
 app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+  res.json(products);
+});
 
 app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(
+  PORT,
+  console.log(`server  running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
