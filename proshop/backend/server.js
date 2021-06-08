6b82b39cc2535942ac8099c5fc1759096e573e23
Selim Agovic <selimagovic@gmail.com>
2021-06-08 23:14:18 +0200

    Added custom Error Handelers

--- proshop/backend/server.js
+++ proshop/backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -11,10 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   console.log('HELLO');
   next();
-});
+});*/
 
 app.get('/', (req, res) => {
   res.send('Api is running...');
@@ -22,14 +23,9 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 
-app.use((err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-  });
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(
   PORT,
