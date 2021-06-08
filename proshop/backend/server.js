e614c563a205303821fbc6c79203767fac6de7f1
Selim Agovic <selimagovic@gmail.com>
2021-06-08 21:42:20 +0200

    added db

--- proshop/backend/server.js
+++ proshop/backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -11,17 +11,24 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('HELLO');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Api is running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
 });
 
 app.listen(
