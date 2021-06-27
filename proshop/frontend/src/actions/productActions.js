2621cb3102e88a8b82ad06b8f04d4af8036fb007
Selim Agovic <selimagovic@gmail.com>
2021-06-27 20:55:53 +0200

    Added Couple of Components
    Added Helper scripts
    Added Constants, Reducers vs

---
+++ proshop/frontend/src/actions/productActions.js
@@ -0,0 +1,23 @@
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCESS,
+  PRODUCT_LIST_FAIL,
+} from '../constants/productConstants.js';
+import axios from 'axios';
+
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+    const { data } = await axios.get('/api/products');
+
+    dispatch({ type: PRODUCT_LIST_SUCESS, payload: data });
+  } catch (err) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
