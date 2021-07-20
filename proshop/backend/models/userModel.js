c9d5cd00137d8a00b38e333be94fe1071e74f6b8
Selim Agovic <selimagovic@gmail.com>
2021-07-20 16:28:44 +0200

    Added AUTH middleware, added utils for token generation -authMiddleware.js -generateToken.js
    Added two more routes for get User Profile and register New user
    
    created method for encripting user password before sending it to database.
    -userModel.js

--- proshop/backend/models/userModel.js
+++ proshop/backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcryp from 'bcryptjs';
 
 const userSchema = mongoose.Schema(
   {
@@ -25,6 +26,18 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcryp.compare(enteredPassword, this.password);
+};
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcryp.genSalt(10);
+  this.password = await bcryp.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 
--- proshop/backend/server.js
+++ proshop/backend/server.js
@@ -2,8 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -11,6 +13,8 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(express.json());
 
 /*app.use((req, res, next) => {
   console.log('HELLO');
@@ -22,6 +26,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 
